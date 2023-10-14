cd9b0ebb01b93fc9e844574bb7c5c4e897aa0bc7
Shubham <bhattacharya.shubham123@gmail.com>
2023-10-14 17:22:24 -0500

    Completed step 2

--- app/src/main/java/edu/iastate/shubham8/random_pets/MainActivity.kt
+++ app/src/main/java/edu/iastate/shubham8/random_pets/MainActivity.kt
@@ -2,10 +2,34 @@ package edu.iastate.shubham8.random_pets
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getDogImageURL()
     }
+
+    private fun getDogImageURL() {
+        val client = AsyncHttpClient()
+        client["https://dog.ceo/api/breeds/image/random", object : JsonHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
+                Log.d("Dog", "response successful")
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                errorResponse: String,
+                throwable: Throwable?
+            ) {
+                Log.d("Dog Error", errorResponse)
+            }
+        }]
+    }
+
 }
