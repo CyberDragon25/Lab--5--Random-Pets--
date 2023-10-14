a41f2638c17a3cd51aac8a3db7b9baafcbeb5057
Shubham <bhattacharya.shubham123@gmail.com>
2023-10-14 17:28:24 -0500

    Completed step 3

--- app/src/main/java/edu/iastate/shubham8/random_pets/MainActivity.kt
+++ app/src/main/java/edu/iastate/shubham8/random_pets/MainActivity.kt
@@ -8,17 +8,20 @@ import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
 
 class MainActivity : AppCompatActivity() {
+    var petImageURL = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         getDogImageURL()
+        Log.d("petImageURL", "pet image URL set")
     }
 
     private fun getDogImageURL() {
         val client = AsyncHttpClient()
         client["https://dog.ceo/api/breeds/image/random", object : JsonHttpResponseHandler() {
             override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
-                Log.d("Dog", "response successful")
+                Log.d("Dog", "response successful$json")
+                petImageURL = json.jsonObject.getString("message")
             }
 
             override fun onFailure(
